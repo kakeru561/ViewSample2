d96d73fb4ccecfde62083bb95ed1dcfc81fe3a68
中村翔 <kakeru.leon.561@gmail.com>
2018-05-17 10:52:47 +0900

    init

---
+++ src/main/java/com/websarva/wings/android/viewsample2/ViewSampleActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.viewsample2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewSampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_sample);
+    }
+}
